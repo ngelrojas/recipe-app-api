1c395f78942d89f096dfdc3bae000ffbc351c8b5
ngelrojasp <aomarrojasp@gmail.com>
2019-06-12 19:42:49 -0300

    models updated

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """"""
+        email = 'me@ngelrojasp.com'
+        password = 'ngelrojasp123'
+
+        user=get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """test the email for a new user is normalized"""
+        email = 'me@NGELROAJSP.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
