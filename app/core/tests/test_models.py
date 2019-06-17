fb469124141c340321bae0c3da6860654755e373
ngelrojasp <aomarrojasp@gmail.com>
2019-06-17 12:17:08 -0300

    update files test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ class ModelTests(TestCase):
         email = 'me@ngelrojasp.com'
         password = 'ngelrojasp123'
 
-        user=get_user_model().objects.create_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
@@ -23,3 +23,19 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """
+        test creating user with no email raises error
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """ test creating new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'me@ngelrojasp.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
