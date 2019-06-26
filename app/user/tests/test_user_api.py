26a18523613cf0718f9539863543554c08a088a9
ngelrojasp <aomarrojasp@gmail.com>
2019-06-26 18:28:17 -0300

    more problems fixed

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -96,10 +96,6 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-        # res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
-        # self.assertNotIn('token', res.data)
-        # self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
     def test_retrieve_user_unauthorized(self):
         """test that authentication is required for users"""
         res = self.client.get(ME_URL)
